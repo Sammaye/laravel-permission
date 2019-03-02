cd9db7ce3dd76e28139d8db6d44c9b476809e257
Sam Millman <sam.millman@googlemail.com>
2019-03-02 18:55:17 +0000

    Added guard for empty schemas

--- src/Providers/PermissionServiceProvider.php
+++ src/Providers/PermissionServiceProvider.php
@@ -6,6 +6,7 @@ use App\Console\Commands\AssignPerrmission;
 use App\Console\Commands\AssignRole;
 use App\Console\Commands\AssignRolePermission;
 use App\Console\Commands\RefreshPermission;
+use Illuminate\Support\Facades\Schema;
 use Sammaye\Permission\Permission;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -13,8 +14,6 @@ use Illuminate\Support\ServiceProvider;
 class PermissionServiceProvider extends ServiceProvider
 {
     /**
-     * Register any authentication / authorization services.
-     *
      * @return void
      */
     public function boot()
@@ -34,16 +33,18 @@ class PermissionServiceProvider extends ServiceProvider
             ]);
         }
 
-        Gate::before(function ($id, $ability) {
-            $user = config('sammaye.permission.user')::where('id', $id)->firstOrFail();
-            return $user->hasPermission('root') ?: null;
-        });
-
-        foreach (Permission::all() as $perm) {
-            Gate::define($perm->name, function ($id) use ($perm) {
+        if (Schema::hasTable((new Permission)->getTable())) {
+            Gate::before(function ($id, $ability) {
                 $user = config('sammaye.permission.user')::where('id', $id)->firstOrFail();
-                return $user->hasPermission($perm->name);
+                return $user->hasPermission('root') ?: null;
             });
+
+            foreach (Permission::all() as $perm) {
+                Gate::define($perm->name, function ($id) use ($perm) {
+                    $user = config('sammaye.permission.user')::where('id', $id)->firstOrFail();
+                    return $user->hasPermission($perm->name);
+                });
+            }
         }
     }
 }
